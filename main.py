e75ed5701d21c0c8cc353b0f2a34cfeab30f8f0e
GeorgeBakalski <george1216.gb@gmail.com>
2025-12-31 17:10:08 +0000

    function call

--- main.py
+++ main.py
@@ -3,23 +3,13 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-def main():
-    
-    if api_key is None:
-        raise RuntimeError("API key not found")
-    
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+def generate_content(client, messages, verbose):
 
-    client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents= messages,
@@ -34,18 +24,48 @@ def main():
     prompt_tokens = metadata.prompt_token_count
     response_tokens = metadata.candidates_token_count
     
-    if args.verbose == True:
-        print(f"User prompt: {args.user_prompt}")
+    if verbose:
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
 
-    if response.function_calls is None:
+    if not response.function_calls:
         print(f"Response: \n{response.text}")
         return 
     
+    function_responses = []
     for function_call in  response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call, verbose)
+
+        if not function_call_result.parts:
+            raise Exception("Function call returned no parts")
+
+        if not function_call_result.parts[0].function_response:
+            raise Exception("Missing FunctionResponse on part")
+        
+        if not function_call_result.parts[0].function_response.response:
+            raise Exception("Empty function response payload")
+        
+        function_responses.append(function_call_result.parts[0])
+
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+
+def main():
     
+    if api_key is None:
+        raise RuntimeError("API key not found")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    client = genai.Client(api_key=api_key)
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
 
 if __name__ == "__main__":
     main()
